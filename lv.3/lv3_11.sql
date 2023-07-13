2b51921ef033006df43493b35f746dc7379630c6
Taeyeon Kim <51779879+taeyeon5362@users.noreply.github.com>
2023-07-14 00:02:18 +0900

    조건에 맞는 사용자 정보 조회하기

---
+++ lv.3/lv3_11.sql
@@ -0,0 +1,13 @@
+SELECT DISTINCT B.USER_ID
+       , B.NICKNAME
+       , B.CITY||' '|| B.STREET_ADDRESS1||' '||B.STREET_ADDRESS2 AS "전체주소"
+       , SUBSTR(TLNO,1,3)||'-'||SUBSTR(TLNO,4,4)||'-'||SUBSTR(TLNO,8) AS "전화번호"
+FROM USED_GOODS_BOARD A JOIN USED_GOODS_USER B
+ON A.WRITER_ID = B.USER_ID
+WHERE B.USER_ID IN (SELECT B.USER_ID
+                    FROM USED_GOODS_BOARD A JOIN USED_GOODS_USER B
+                    ON A.WRITER_ID = B.USER_ID
+                    GROUP BY B.USER_ID
+                    HAVING COUNT(*) >= 3
+                   )
+ORDER BY B.USER_ID DESC;
